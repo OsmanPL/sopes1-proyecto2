749409e86a2535fa938c52a8d0434f834ec484b8
Osman Pérez <2999565980101@ingenieria.usac.edu.gt>
2021-11-12 11:03:54 -0600

    kafka

--- kafka/api/go.mod
+++ kafka/api/go.mod
@@ -6,18 +6,27 @@ require (
 	github.com/KromDaniel/rejonson v0.0.0
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/gorilla/mux v1.8.0
-	github.com/joho/godotenv v1.4.0
 	github.com/segmentio/kafka-go v0.4.23
+	go.mongodb.org/mongo-driver v1.7.4
 	google.golang.org/grpc v1.41.0
 	google.golang.org/protobuf v1.27.1
 )
 
 require (
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
-	github.com/klauspost/compress v1.9.8 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/net v0.0.0-20211101193420-4a448f8816b3 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/sys v0.0.0-20211101204403-39c9dd37992c // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20211101144312-62acf1d99145 // indirect
--- kafka/api/worker/worker.go
+++ kafka/api/worker/worker.go
@@ -6,6 +6,11 @@ import(
 	"fmt"
 	"log"
 	"os"
+	"time"
+	"encoding/json"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"github.com/KromDaniel/rejonson"
 	"github.com/go-redis/redis"
 )
@@ -14,9 +19,25 @@ const(
 	topic = "Game"
 	brokerAddress = "localhost:9092"
 )
+var (
+	usr      = "admin"
+	pwd      = "HQ03ilp&dOtj"
+	host     = "34.132.83.52"
+	port     = 27017
+	database = "squid_game"
+)
 var goRedisClient = redis.NewClient(&redis.Options{
 	Addr: "34.121.89.39:6379",
   })
+  
+
+  type Game struct {
+	ID       int    `json:"id"`
+	GameName string `json:"gameName"`
+	Winner   string `json:"winner"`
+	Players  int    `json:"players"`
+	Worker   string `json:"worker"`
+}
 
 
 func consume(ctx context.Context){
@@ -35,7 +56,8 @@ func consume(ctx context.Context){
 		}
 
 		fmt.Println("Mensaje recibido: ", string(msg.Value))
-		insertRedis(string(msg.Value))
+		go insertRedis(string(msg.Value))
+		go insertMongo(ctx,string(msg.Value))
 	}
 }
 
@@ -43,6 +65,37 @@ func insertRedis(body string){
 	client := rejonson.ExtendClient(goRedisClient)
 	client.JsonArrAppend("squid_game",".",body)
 }
+
+func insertMongo(ctx context.Context,body string){
+	var game Game
+	json.Unmarshal([]byte(body), &game)
+	var collection = GetCollection("games")
+	var err error
+	_, err = collection.InsertOne(ctx, game)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func GetCollection(collection string) *mongo.Collection {
+	uri := fmt.Sprintf("mongodb://%s:%s@%s:%d", usr, pwd, host, port)
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return client.Database(database).Collection(collection)
+}
+
 
 func main(){
 	ctx := context.Background()
